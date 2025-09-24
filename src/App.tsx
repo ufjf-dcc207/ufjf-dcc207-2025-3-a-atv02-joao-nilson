c7e7566bf3af2925dc8164fe180bb31aa6a3fd96
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-09-23 22:11:35 -0300

    :sparkles: texto

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Disciplina from './disciplina';
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
       <h2>oii react!</h2>
+      <Disciplina></Disciplina>
     </>
   );
 }
---
+++ src/disciplina.tsx
@@ -0,0 +1,8 @@
+export default function Disciplina () {
+    return (
+        <>
+            <h1>DCC207</h1>
+            <h2>Desenvolvimento web frontend</h2>
+        </>
+    )
+}
