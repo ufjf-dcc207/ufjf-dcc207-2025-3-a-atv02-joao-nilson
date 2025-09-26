baa8cc1e9e7497463f430d7d7b61a7db9067796f
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-09-25 21:41:06 -0300

    :art: adiciona estilo ao projeto

--- src/disciplina.tsx
+++ src/disciplina.tsx
@@ -1,8 +1,10 @@
+import "./Disciplina.css"
+
 export default function Disciplina () {
     return (
-        <>
+        <div className="disciplina">
             <h1>DCC207</h1>
             <h2>Desenvolvimento web frontend</h2>
-        </>
+        </div>
     )
 }
